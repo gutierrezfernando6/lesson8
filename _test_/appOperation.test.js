c59304d39ac450c6dee77ad989e9086beba0d253
gutierrezfernando6 <gutierrez.fernando6@gmail.com>
2025-11-25 12:53:20 -0500

    fixed test cases and code coverage

--- _test_/appOperation.test.js
+++ _test_/appOperation.test.js
@@ -1,5 +1,9 @@
 const appOperations = require('../src/appOperations');
 
 test("multiplication property of zero", ()=> {
-    expect(appOperations(5, 0)).toBe(0); 
+    expect(appOperations.multiply(5, 0)).toBe(0); 
+});
+
+test("addition of two numbers", () => {
+    expect(appOperations.add(5,5)).toBe(10);
 });
